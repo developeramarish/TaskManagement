dbed3d35cc9e552f69df55e5f079f997c464e128
Adrian <adrian.ardelean95@gmail.com>
2017-07-14 15:24:58 +0300

    First commit. Finished Ninject Configuration.

---
+++ src/WebApi2Book.Web.Api/App_Start/WebApiConfig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Dispatcher;
+using System.Web.Http.Routing;
+using WebApi2Book.Web.Common;
+using WebApi2Book.Web.Common.Routing;
+
+namespace WebApi2Book.Web.Api
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+
+            var constraintsResolver = new DefaultInlineConstraintResolver();
+            constraintsResolver.ConstraintMap.Add("apiVersionConstraint", typeof(ApiVersionConstraint));
+
+
+            config.MapHttpAttributeRoutes(constraintsResolver);
+
+            config.Services.Replace(typeof(IHttpControllerSelector), new NamespaceHtppControllerSelector(config));
+
+            // Web API configuration and services
+
+            // Web API routes
+            //config.MapHttpAttributeRoutes();
+
+            //Matches route with the taskNum parameter
+            //config.Routes.MapHttpRoute(
+            //        name: "FindByTaskNumberRoute",
+            //        routeTemplate: "api/{controller}/{taskNum}",
+            //        defaults: new { taskNum = RouteParameter.Optional }
+            //);
+
+            ////Default catch-all
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+            }
+        
+    }
+}
---
+++ src/WebApi2Book.Web.Api/Controllers/TasksController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi2Book.Web.Api.Controllers
+{
+    [RoutePrefixAttribute("api/employeeTasks")]
+    public class TasksController : ApiController
+    {
+        [Route("{id:int:max(100)}")]
+        public string GetTaskWithAMaxIdOf100(int id)
+        {
+            return "In the GetTaskWithAMaxIdOf100(int id) method, id = " + id;
+        }
+
+        [Route("{id:int:min(101)}")]
+        [HttpGet]
+        public string FindTasksWithAMinIdOf101(int id)
+        {
+            return "In the FindTasksWithAMinIdOf101(int id) method, id = " + id;
+        }
+
+        public string Get(string taskNum)
+        {
+            return "In the Get(string taskNum) method, taskNum = " + taskNum;
+        }
+    }
+}
