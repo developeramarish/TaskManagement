57b8a0f0c9626e1e7274cdeb48e8e7ff6b10e56c
Adrian <adrian.ardelean95@gmail.com>
2017-07-21 16:41:10 +0300

    Setup mapping classes.

---
+++ src/WebApi2Book.Data.SqlServer/Mapping/UserMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi2Book.Data.Entities;
+
+namespace WebApi2Book.Data.SqlServer.Mapping
+{
+    public class UserMap : VersionedClassMap<User>
+    {
+        public UserMap()
+        {
+            Id(x => x.UserId);
+            Map(x => x.FirstName).Not.Nullable();
+            Map(x => x.LastName).Not.Nullable();
+            Map(x => x.Username).Not.Nullable();
+        }
+    }
+}
